bbeb38ca545284bf0d7c4548ba43e19c504e86c4
xianyuxiaoqiang <shanshu@cn.fujitsu.com>
2016-01-20 17:51:22 +0800

    Update demo.c
    1. delete minus power (except for the rejection input)
    2. grouping for every expected number( one group of networks for one number)
    3. for muti PNG files in folder
    4. use input matrix for performance update, also for rejection
    5. update recogize test function

--- NeuralNetworks/demo.c
+++ NeuralNetworks/demo.c
@@ -150,7 +150,7 @@ int random_population_neuralnetwork( population_neuralnetwork_t *popu_p, int use
 	unsigned int i,j,k;
 	neuralnetwork_t *chromo_p;
 	int randnum = 1000; // todo : change this number. It is importent!
-	double magic = 0.001;
+	double magic = 0.0001;
 	int ran;
 	char namebuf[20];
 
@@ -164,12 +164,13 @@ int random_population_neuralnetwork( population_neuralnetwork_t *popu_p, int use
 				ran = getRand(randnum);
 				if( ran < randnum / 2 )
 				{
-                    ran *= -1;
+                    // ran *= -1; // no minus power
 				}
 				else
 				{
 					ran -= randnum / 2;
 				}
+				if( ran == 0 )ran++;
 				chromo_p->hide_neural_p[j].power_p[k] = magic * ran; // the magic number
 			}
 		}
@@ -180,13 +181,15 @@ int random_population_neuralnetwork( population_neuralnetwork_t *popu_p, int use
 				ran = getRand(randnum);
 				if( ran < randnum / 2 )
 				{
-                    ran *= -1;
+                    // ran *= -1;// no minus power
 				}
 				else
 				{
 					ran -= randnum / 2;
 				}
-				chromo_p->output_neural_p[j].power_p[k] = magic * ran;
+				if( ran == 0 )ran++;
+				// chromo_p->output_neural_p[j].power_p[k] = magic * ran;
+				chromo_p->output_neural_p[j].power_p[k] = 1;
 			}// performance update
 		}
 	}
@@ -265,17 +268,30 @@ int active_neuralnetwork( population_neuralnetwork_t *popu_p, input_data_recogni
 	neuralnetwork_t *chromo_p;
 	population_neuralnetwork_t next_popu;
 	// caculate score of all individual
+	memset(popu_p->group_success_flag, 0, sizeof(popu_p->group_success_flag));
 	for( i=0; i<popu_p->individual_num; i++ )
 	{
 		ret = updateScore_neuralnetwork(&popu_p->individual_array[i], data_p, data_num);
 		
 		if( ret == 1 )
 		{
-			debug_output("active: success!");
-			return 1;
+			if( popu_p->group_success_flag[popu_p->individual_array[i].group_id-1] == 0 )
+			{
+				popu_p->group_success_flag[popu_p->individual_array[i].group_id-1] = 1;
+				max_index++;
+			}
+		}
+		if( popu_p->group_success_flag[popu_p->individual_array[i].group_id-1] == 0 )
+		{
+			tryRemember_neuralnetwork(&popu_p->individual_array[i], data_p, data_num);
 		}
 	}
-
+	
+	if( max_index == GROUP_NUMBER )
+	{
+		debug_output("active: success!");
+		return 1;
+	}
 	crossover_neuralnetwork( popu_p, data_p, data_num );
 
 	mutation_neuralnetwork( popu_p, data_p, data_num );
@@ -397,15 +413,39 @@ void recognize_number_test(char *inputfile, neuralnetwork_t *chromo_p)
 }
 void recognize_number_test_in_png(char *inputfile, neuralnetwork_t *chromo_p)
 {
-	int i;
 	double out;
 	input_data_recognition_t data;
 	data = initInputDataRecPNG(inputfile);
 	memcpy(chromo_p->input_data_p, &data.data, sizeof(data.data) );
 	out = getOutputOfNeuralnetwork(chromo_p);
+	if( chromo_p->group_id != ((out - (int)out) > 0.5 ? (int)out + 1 : (int)out) )
+	{
+		// TODO :output refuse msg
+		return;
+	}
 	printf( "I guess the number is : %d\n", 
 		((out - (int)out) > 0.5 ? (int)out + 1 : (int)out)
 		);
+}
+void recognize_number_test_in_png_in_folder(char *folder, neuralnetwork_t *chromo_p, input_data_recognition_t* data_p, int data_max_num)
+{
+	double out;
+	int data_num;
+	int i;
+	data_num = initInputDataRecPNG_ex(folder, data_p, 0, data_max_num-1);
+	for( i=0; i<data_num; i++ )
+	{
+		memcpy(chromo_p->input_data_p, &data_p[i].data, sizeof(data_p[i].data) );
+		out = getOutputOfNeuralnetwork(chromo_p);
+		if( chromo_p->group_id != ((out - (int)out) > 0.5 ? (int)out + 1 : (int)out) )
+		{
+			// TODO :output refuse msg
+			continue;
+		}
+		printf( "I guess the number is : %d, and the expected value is : %f\n", 
+			((out - (int)out) > 0.5 ? (int)out + 1 : (int)out) , data_p[i].expcet
+			);
+	}
 }
 // test numbers from 1 to 9 in static text files
 int test_static_numbers( int argc, char ** argv )
@@ -463,26 +503,125 @@ int test_static_numbers( int argc, char ** argv )
 
     return 0;
 }
+void update_power_by_input_reject_matrix( population_neuralnetwork_t *popu_p, input_data_recognition_t *data_p, int data_num )
+{
+	int i,k;
+	int row, col;
+	char filename[100];
+	FILE *fp;
+	int tmp;
+	for( i=0; i<popu_p->individual_num; i++ )
+	{
+		sprintf( filename, "input_reject_matrix%d.txt", popu_p->individual_array[i].group_id );
+		fp = fopen( filename, "r" );
+		if( fp )
+		{
+			fscanf(fp, "### input matrix  ###\n");
+			for( row=0; row < DATA_INPUT_ROW_NUM; row++ )
+			{
+				for( col=0; col < DATA_INPUT_COL_NUM; col++ )
+				{
+					fscanf(fp, "%02d ", &tmp);
+					if( tmp == 0 )
+					{
+						// reject
+						for( k=0; k<popu_p->individual_array[i].hide_neural_num; k++ )
+						{
+							popu_p->individual_array[i].hide_neural_p[k].power_p[row * DATA_INPUT_COL_NUM + col] = MINUS_MAX;
+						}
+					}
+				}
+				fscanf(fp, "\n");
+			}
+			fclose(fp);
+		}
+	}
+}
+void print_input_reject_matrix( input_data_recognition_t *data_p, int data_num )
+{
+	int i,j;
+	int row, col;
+	char filename[100];
+	FILE *fp;
+	double tmp;
+	for( i=0; i<10; i++ )
+	{
+		sprintf( filename, "input_reject_matrix%d.txt", i+1 );
+		fp = fopen( filename, "w" );
+		if( fp )
+		{
+			fprintf(fp, "### input matrix  ###\n");
+			for( row=0; row < DATA_INPUT_ROW_NUM; row++ )
+			{
+				for( col=0; col < DATA_INPUT_COL_NUM; col++ )
+				{
+					tmp = 0;
+					for( j=0; j<data_num; j++ )
+					{
+						if( data_p[j].expcet == i+1 )
+						{
+							tmp += data_p[j].data[row * DATA_INPUT_COL_NUM + col];
+						}
+					}
+					fprintf(fp, "%02d ", (int)tmp);
+				}
+				fprintf(fp, "\n");
+			}
+			fprintf(fp, "### reject matrix ###\n");
+			for( row=0; row < DATA_INPUT_ROW_NUM; row++ )
+			{
+				for( col=0; col < DATA_INPUT_COL_NUM; col++ )
+				{
+					tmp = 0;
+					for( j=0; j<data_num; j++ )
+					{
+						if( data_p[j].expcet != i+1 )
+						{
+							tmp += data_p[j].data[row * DATA_INPUT_COL_NUM + col];
+						}
+					}
+					fprintf(fp, "%02d ", (int)tmp);
+				}
+				fprintf(fp, "\n");
+			}
+		    fclose(fp);
+		}
+	}
+}
+
 int main( int argc, char ** argv )
 {
 	population_neuralnetwork_t popu;
 	int i;
 	int ret;
-	input_data_recognition_t data[10];
+	input_data_recognition_t *data_p;
 	char filename[100];
+	int input_data_max = 1000;
+	int input_data_num = 0;
 	
+	data_p = (input_data_recognition_t*)malloc( sizeof(input_data_recognition_t) * input_data_max );
+	
+	input_data_num = initInputDataRecPNG_ex(NULL, data_p, 0, input_data_max-1);
+	print_input_reject_matrix( data_p, input_data_num );
+	if( input_data_num == 0 )
+	{
+		return 0;
+	}
+
 	init_population_neuralnetwork( &popu, 100);
-	random_population_neuralnetwork( &popu, FALSE );
-	//random_population_neuralnetwork( &popu, TRUE );
-	for( i=0; i<10; i++ )
+
+	update_power_by_input_reject_matrix( &popu, data_p, input_data_num );
+	//random_population_neuralnetwork( &popu, FALSE );
+	random_population_neuralnetwork( &popu, TRUE );
+	/*for( i=0; i<10; i++ )
 	{
 		sprintf(filename, "%d.png",i+1);
-		data[i] = initInputDataRecPNG(filename);
-	}
+		data_p[i] = initInputDataRecPNG(filename);
+	}*/
 
     for( i=0; i<MAX_GENERATION; i++ )
 	{
-		ret = active_neuralnetwork( &popu, data, 10 );
+		ret = active_neuralnetwork( &popu, data_p, input_data_num );
 		if( ret == 1 )
 		{
 			printf("success at %d genaration.\n", i+1);
@@ -492,28 +631,66 @@ int main( int argc, char ** argv )
 	}
 	if( ret == 1 )
 	{
-		for( i=0; i<popu.individual_num; i++ )
+		int data_num;
+		int m;
+		double out;
+		int guess, expected;
+		int right = 0, wrong = 0, refuse = 0;
+		data_num = initInputDataRecPNG_ex("recognization", data_p, 0, input_data_max-1);
+		saveMaxScoreNeuralnetwork( &popu, "save.log" );
+		for( m=0; m<data_num; m++ )
 		{
-			if( popu.individual_array[i].score >= SUCCESS_SCORE )
+			for( i=0; i<popu.individual_num; i++ )
 			{
-				saveMaxScoreNeuralnetwork( &popu, "save.log" );
-				showResulNeural(&popu.individual_array[i]);
-				recognize_number_test_in_png("1.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("2.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("3.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("4.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("5.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("6.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("7.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("8.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("9.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("10.png", &popu.individual_array[i]);
-				recognize_number_test_in_png("Recognition.png", &popu.individual_array[i]);
-				break;
+				if( popu.individual_array[i].score >= SUCCESS_SCORE )
+				{					
+					memcpy(popu.individual_array[i].input_data_p, &data_p[m].data, sizeof(data_p[m].data) );
+					out = getOutputOfNeuralnetwork(&popu.individual_array[i]);
+					guess = (out - (int)out) > 0.5 ? (int)out + 1 : (int)out;
+					expected = (int)data_p[m].expcet;
+					if( popu.individual_array[i].group_id != guess )
+					{
+						// TODO :output refuse msg
+						continue;
+					}
+					printf( "guess : %d, expected : %d(actual out=%f)\n", 
+						guess , expected, out
+						);
+					if( guess == expected )
+					{
+						right++;
+					}
+					else
+					{
+						wrong++;
+					}
+					break;
+					// showResulNeural(&popu.individual_array[i]);
+					// recognize_number_test_in_png_in_folder("recognization", &popu.individual_array[i], data_p, input_data_max);
+					/* recognize_number_test_in_png("1.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("2.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("3.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("4.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("5.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("6.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("7.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("8.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("9.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("10.png", &popu.individual_array[i]);
+					recognize_number_test_in_png("Recognition.png", &popu.individual_array[i]); */
+					// break;
+				}
 			}
 		}
+		refuse = data_num - right - wrong;
+		printf("summerize : \n");
+		printf("   total  : %d\n", data_num);
+		printf("   right  : %d\n", right);
+		printf("   wrong  : %d\n", wrong);
+		printf("   refuse : %d\n", refuse);
 	}
 	free_population_neuralnetwork(&popu);
+	free(data_p);
     return 0;
 }
 void test_gene()
